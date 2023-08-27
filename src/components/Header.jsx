6590bfa6796155a3747bcec81ea20cbf2020c84f
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-08-27 14:34:05 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    
+    return (
+        <h2 className="text-4xl font-extrabold text-gray-500 text-center">
+            ¿Cuanto <span className="text-indigo-600">dinero </span> necesitas?
+        </h2>
+    )
+}
+
+export default Header;
